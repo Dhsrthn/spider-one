4b1c5ca69b261eb286b4d7b30761abfebf0be171
Dhsrthn <dhasarathan120904@gmail.com>
2023-06-14 00:31:29 +0530

    commit cuz

--- puzzle.js
+++ puzzle.js
@@ -1,9 +1,3 @@
-let col,row
-
-let maxX=3
-let maxY=3
-let variablechange
-let directionchange
 const puzzleboard=document.getElementById('puzzle')
 swapped=false
 let swapx,swapy
@@ -11,74 +5,9 @@ let fromindex
 let toindex
 let temp
 
-let randomized=false
-
 const randombutton=document.getElementById('pleaserandom')
-const randombutton2=document.getElementById('randomalso')
 
-one=document.createElement('img')
-one.src='files/1.png'
-two=document.createElement('img')
-two.src='files/2.png'
-three=document.createElement('img')
-three.src='files/3.png'
-four=document.createElement('img')
-four.src='files/4.png'
-five=document.createElement('img')
-five.src='files/5.png'
-six=document.createElement('img')
-six.src='files/6.png'
-seven=document.createElement('img')
-seven.src='files/7.png'
-eight=document.createElement('img')
-eight.src='files/8.png'
-empty=document.createElement('img')
-empty.src='files/empty.png'
-
-numberarray=[one,two,three,four,five,six,seven,eight,empty]
-let array=[[1,1,numberarray[0]],[1,2,numberarray[1]],[1,3,numberarray[2]],[2,1,numberarray[3]],[2,2,numberarray[4]],[2,3,numberarray[5]],[3,1,numberarray[6]],[3,2,numberarray[7]],[3,3,numberarray[8]]]
-
-
-function main(){
-
-    gameloop()
-    requestAnimationFrame(main)
-}
-
-window.requestAnimationFrame(main)
-
-window.addEventListener('mousedown',clicked)
-
-function clicked(event) {
-    const puzzleRect = puzzleboard.getBoundingClientRect();
-    const cellWidth = puzzleRect.width / 3;
-    const cellHeight = puzzleRect.height / 3;
-    if(event.clientX>puzzleRect.left && event.clientX<puzzleRect.left+puzzleRect.width && event.clientY>puzzleRect.top && event.clientY<puzzleRect.top+puzzleRect.height ){
-        swapped=false
-        const offsetX = event.clientX - puzzleRect.left;
-        const offsetY = event.clientY - puzzleRect.top;
-        col = Math.floor(offsetX / cellWidth) + 1;
-        row = Math.floor(offsetY / cellHeight) + 1;
-  }
-  if(!swapped){
-    swap()
-  }
-  if(event.target==randombutton || event.target==randomalso ){
-    console.log('hello')
-    if(!randomized){
-        for(let i=0;i<70;i++){
-            randomizer()
-        }
-          
-        randomized=true
-      }
-  }
-  
-  
-}
-
-
-
+let cellHeight,cellWidth
 
 function gameloop(){
     puzzleboard.innerHTML = '';
@@ -86,85 +15,205 @@ function gameloop(){
         const wordelement = document.createElement('div')
         wordelement.style.gridColumnStart=array[i][1]
         wordelement.style.gridRowStart=array[i][0]
-        array[i][2].classList.add('puzzle-image')
-        wordelement.appendChild(array[i][2])
+        array[i][2][0].classList.add('puzzle-image')
+        wordelement.appendChild(array[i][2][0])
         wordelement.classList.add('puzzle-cell')
         puzzleboard.appendChild(wordelement)
         
     }
 }
 
-function swap(){
-    for(let i=0;i<9;i++){
-        if(array[i][2]==empty){
-            swapx=array[i][0]
-            swapy=array[i][1]
-            toindex=i
+let startingnow=true
+let numberpuzzle=true
+let gridrows=3
+let gridcolumns=3
+let arrayofnumbers=[]
+let arrayforpuzzle=[]
+let starting
+let totalboxes
+let fontsize
+let swaprow,swapcol
+
+const noinpuzzles=document.getElementsByClassName('puzzle-number')
+function main(){
+    const puzzleRect = puzzleboard.getBoundingClientRect();
+    cellWidth = puzzleRect.width / gridcolumns;
+    cellHeight = puzzleRect.height / gridrows;
+    if(numberpuzzle==true){
+        if(startingnow){
+            numberpuzzleshow()
+        }
+        displaypuzzlenumber()
+        
+    }
+    //gameloop()
+    requestAnimationFrame(main)
+}
+
+window.requestAnimationFrame(main)
+
+function numberpuzzleshow(){
+    starting=0
+    totalboxes=gridcolumns*gridrows
+    //number of colmuns is x
+    //number of rows is y
+    puzzleboard.style.gridTemplateColumns = `repeat(${gridcolumns}, 1fr)`;
+    puzzleboard.style.gridTemplateRows = `repeat(${gridrows}, 1fr)`;
+    for(let i=0;i<totalboxes;i++){
+        if(i==(totalboxes)-1){
+            arrayofnumbers[i]=' '
+        }
+        else{
+            arrayofnumbers[i]=i+1
         }
     }
-    if((row-swapx)+(col-swapy)==1 || (row-swapx)+(col-swapy)==-1){
-        array.forEach((combo)=>{
-            if(combo[0]==row && combo[1]==col){
-                fromindex=array.indexOf(combo)
-            }
-        })
-        actuallyswap(fromindex,toindex)
+    for(let i=1;i<gridrows+1;i++){
+        for(let j=1;j<gridcolumns+1;j++){
+            arrayforpuzzle[starting]=[i,j,arrayofnumbers[starting]]
+            starting++
+        }
     }
-    swapped=true
+    displaypuzzlenumber()
+    startingnow=false
+}
+
+function displaypuzzlenumber(){
+    puzzleboard.style.backgroundImage = "url('files/puzzleback.jpg')"
+    puzzleboard.innerHTML = '';
+    for(let i=0;i<totalboxes;i++){
+        const wordelement = document.createElement('div')
+        wordelement.style.gridColumnStart=arrayforpuzzle[i][1]
+        wordelement.style.gridRowStart=arrayforpuzzle[i][0]
+        wordelement.innerHTML=arrayforpuzzle[i][2]
+        wordelement.classList.add('puzzle-number')
+        
+        puzzleboard.appendChild(wordelement)
+    }
+    fontsize=Math.min(cellHeight/3,cellWidth/3)
+    for (let i = 0; i < noinpuzzles.length; i++) {
+        noinpuzzles[i].style.fontSize = fontsize+'px';
+    }
+    
+    
+}
+
+window.addEventListener('mousedown',clicked)
+
+function clicked(event) {
+    const puzzleRect = puzzleboard.getBoundingClientRect();
+    if(event.clientX>puzzleRect.left && event.clientX<puzzleRect.left+puzzleRect.width && event.clientY>puzzleRect.top && event.clientY<puzzleRect.top+puzzleRect.height ){
+        swapped=false
+        const offsetX = event.clientX - puzzleRect.left;
+        const offsetY = event.clientY - puzzleRect.top;
+        col = Math.floor(offsetX / cellWidth) + 1;
+        row = Math.floor(offsetY / cellHeight) + 1;
+        if(!swapped){
+            swap()
+          }
+  }
+  
+  if(event.target==randombutton ){
+        actualrandomizer()
+  } 
+}
+
+function swap(){
+    if(!numberpuzzle){
+        
+    }
+    if(numberpuzzle){
+        for(let i=0;i<totalboxes;i++){
+            if(arrayforpuzzle[i][2]==' '){
+                swaprow=arrayforpuzzle[i][0]
+                swapcol=arrayforpuzzle[i][1]
+                toindex=i
+            }
+        }
+        if(((row==swaprow+1 || row==swaprow-1) && col==swapcol) || (row==swaprow && (col==swapcol+1 || col==swapcol-1) )){
+            arrayforpuzzle.forEach((combo)=>{
+                if(combo[0]==row && combo[1]==col){
+                    fromindex=arrayforpuzzle.indexOf(combo)
+                }
+            })
+            actuallyswap(fromindex,toindex)
+        }
+
+    }
+    
 }
 
 function actuallyswap(index1,index2){
-        temp=array[index1][2]
-        array[index1][2]=array[index2][2]
-        array[index2][2]=temp
+    if(numberpuzzle){
+        temp=arrayforpuzzle[index1][2]
+        arrayforpuzzle[index1][2]=arrayforpuzzle[index2][2]
+        arrayforpuzzle[index2][2]=temp
+
+    }
+    if(!numberpuzzle){
+        
+    }   
+    
 }
 
-let startx,starty,startindex,tocol,torow,endindex
+let tempnumberarray=[]
+let len
+let tempindex
+let checkingarray=[]
+let numberofinverse
 
-function randomizer(){
-    variablechange=Math.floor(Math.random()*2)+1
-    directionchange=Math.floor(Math.random()*2)+1
-    for(let i=0;i<9;i++){
-        if(array[i][2]==empty){
-            startx=array[i][0]
-            starty=array[i][1]
-            startindex=i
-        }
+function actualrandomizer(){
+    if(!numberpuzzle){
+        
     }
-
-    if(variablechange==1){
-        if(directionchange==1){
-            torow=startx+1
-            tocol=starty
+    if(numberpuzzle){
+        checkingarray=[]
+        for(let i=0;i<totalboxes;i++){
+            tempnumberarray[i]=arrayofnumbers[i]
         }
-        if(directionchange==2){
-            torow=startx-1
-            tocol=starty
-        }
-    }
-    if(variablechange==2){
-        if(directionchange==1){
-            tocol=starty+1
-            torow=startx
-        }
-        if(directionchange==2){
-            tocol=starty-1
-            torow=startx
-        }
-    }
-    if(torow>maxX || tocol>maxY){
-        randomizer()
-    }
-    else{
-        for(let j=0;j<9;j++){
-            if(array[j][0]==torow && array[j][1]==tocol){
-                endindex=j
-            }
+        for(let i=0;i<totalboxes;i++){
+            len=tempnumberarray.length
+            tempindex=Math.floor(Math.random()*len)
+            arrayforpuzzle[i][2]=tempnumberarray[tempindex]
+            tempnumberarray.splice(tempindex,1)
         }
        
-        actuallyswap(startindex,endindex)
-        return
+        for(let i=0;i<totalboxes;i++){
+            checkingarray[i]=arrayforpuzzle[i][2]
+        }
+        numberofinverse=numberofinversions(checkingarray)
+        if(numberofinverse%2!==0){
+            actualrandomizer()
+        }
+        else{
+            console.log(arrayforpuzzle)
+            console.log(checkingarray)
+            console.log(numberofinverse)
+            return
+        }
+        
     }
-    
-  
+   
+}
+let ninverse
+let splicingindex
+
+function numberofinversions(anyarray){
+    ninverse=0
+        for(let i=0;i<anyarray.length;i++){
+            if(anyarray[i]==0){
+                splicingindex=i
+            }
+        }
+        anyarray.splice(splicingindex,1)
+        for(let i=0;i<anyarray.length;i++){
+                for(let j=i+1;j<anyarray.length;j++){
+                   
+                        if(anyarray[i]>anyarray[j]){
+                            ninverse+=1
+                        }
+                    
+                   
+                }
+        }
+    return ninverse
 }
